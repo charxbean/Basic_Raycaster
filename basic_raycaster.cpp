6486e64710243671fdd851c6fd455950d3e7de0f
Char <87100424+charxbean@users.noreply.github.com>
2025-02-25 11:15:55 -0600

    Error checking

--- basic_raycaster.cpp
+++ basic_raycaster.cpp
@@ -7,7 +7,7 @@
 #include <cctype>
 #include <algorithm>
 
-//vector type for: eye, viewdir, updir
+//vector type
 typedef struct{
     float i, j, k;
 } VectorType;
@@ -22,11 +22,12 @@ int px_height;
 //arbitrary d value
 int d = 500;
 
-//color type for bg color and material color
+//color type
 typedef struct{
     float r, g, b;
 } ColorType;
 
+//material type 
 typedef struct{
     ColorType Od;
     ColorType Os;
@@ -56,20 +57,17 @@ typedef struct{
     float intensity;
 }LightType;
 
+//depth cue 
 typedef struct{
     ColorType depth_color;
     float a_min, a_max, dist_near, dist_far;
 }DepthCue;
 
+//return type of the trace_ray function
 typedef struct{
-    LightType light;
-    float c1, c2, c3;
-}AttlightType;
-
-typedef struct{
-    int sphere_index;
-    Raytype ray;
-    float t;
+    int sphere_index; //intersected sphere
+    Raytype ray; //ray that intersects with the sphere
+    float t; //the t value of the intersection
 }Intersection;
 
 //initialize variables for the input
@@ -80,7 +78,26 @@ ColorType bkgcolor;
 MaterialColor mtlcolor;
 LightType light;
 DepthCue depth;
-AttlightType attlight;
+
+//struct to check if all neccessary input was given
+typedef struct{
+    bool eye_ = false;
+    bool viewdir_= false;
+    bool updir_= false;
+    bool bkgcolor_= false;
+    bool vfov_= false;
+    bool px_width_= false;
+    bool px_height_= false;
+}ValidInput;
+
+ValidInput valid_input;
+//boolean to check if other inputs were given
+bool sphere_ = false;
+bool material_ = false;
+bool depth_cue = false;
+
+//a_dc value for depth cue
+float a_dc;
 
 //array of material colors
 std::vector<MaterialColor> materialArray;
@@ -107,6 +124,7 @@ VectorType crossProduct(const VectorType& v1, const VectorType& v2) {
 float dotProduct(const VectorType& v1, const VectorType& v2) {
     return (v1.i * v2.i) + (v1.j * v2.j) + (v1.k * v2.k);
 }
+
 //returns a new vector that is the sum of vectors v1 and v2
 VectorType vectorAdd(const VectorType v1, const VectorType v2){
     VectorType newVec;
@@ -117,7 +135,7 @@ VectorType vectorAdd(const VectorType v1, const VectorType v2){
     return newVec;
 }
 
-//returns a new vector that is the difference of vectors v1 and v2
+//returns a new vector that is v1 - v2
 VectorType vectorSubtract(const VectorType v1, const VectorType v2){
     VectorType newVec;
     newVec.i = v1.i - v2.i;
@@ -240,7 +258,7 @@ bool colorEquals(ColorType c1, ColorType c2){
     }
 }
 
-//makes sure a color's rgb values stay between 0 and 1
+//make sure a color's rgb values stay between 0 and 1
 void clampColor(ColorType &c){
     if(c.r > 1){
         c.r = 1;
@@ -269,6 +287,7 @@ void printVector(VectorType v1){
     std::cout << "<" << v1.i << ", " << v1.j << ", " << v1.k << ">" << std::endl;
 }
 
+//prints a color to the terminal
 void printColor(ColorType color){
     std::cout << "r: " << color.r << ", g: " << color.g << ", b: " << color.b << std::endl;
 }
@@ -278,125 +297,108 @@ VectorType vector_N;
 VectorType vector_L;
 VectorType vector_H;
 VectorType vector_V;
-bool light_att = false;
-bool depth_cue = false;
-float a_dc;
 
-float t_shaded;
-
-bool tracing_shadow = false;
-
+//initialize traceRay so it can be used inside ShadeRay
 Intersection traceRay(Raytype ray, int self_index);
 
-//takes the index of a sphere in the sphere array and returns the material color for that sphere
+//shadeRay function takes the index of a sphere in the sphere array, the ray that intersected it, and the t value from the intersection
+//returns the color for that pixel computed using the Blinn-Phong method
 ColorType shadeRay(int sphere_index, Raytype ray, float t){
     
+    //if t < 0 then no object intersection was found, return the background color
     if(t < 0){
         return bkgcolor;
     }
 
+    //find the material that corresponds with the given sphere
     int mat_index = sphereArray[sphere_index].m;
     MaterialColor material = materialArray[mat_index];
     SphereType sphere = sphereArray[sphere_index];
+
+    //the intersection point of the sphere from the eye
     VectorType intersection = vectorAdd(ray.origin, vectorScalar(ray.intersection, t));
 
     //Calculate illumination 
-    //calculate N, L and H
+    //calculate vector N
     vector_N = vectorDivide(vectorSubtract(intersection, sphere.center), sphere.r);
     normalize(vector_N);
 
-    //calculate L for each point light source
+    //calculate vector L for each point light source
     for(int k = 0; k < lightArray.size(); k++){
         if(lightArray[k].type == 1){
-            //calculate light_position - surface position
+            //(light_position - surface position)
             VectorType new_L = vectorSubtract(lightArray[k].position, intersection);
             //calculate vector L
             vector_L = vectorDivide(new_L, vectorLength(new_L));
             normalize(vector_L);
+            //add into vector L array corresponding to the index of this light in the light array
             L_array.insert(L_array.begin() + k, vector_L);
         }
     }
-
 
     //Define V
     vector_V = vectorDivide(vectorSubtract(ray.origin, ray.intersection), vectorLength(vectorSubtract(ray.origin, ray.intersection)));
     normalize(vector_V);
     
-    //initialize intensity
+    //initialize intensity to the ambient light
     ColorType illumination = colorMultiply(material.Od, material.ka); //ka + Od
 
-    //for each light source, compute it's intensity and add to the total intensity value
+    //for each light source, compute it's individual intensity and add to the total intensity value
     for(int k = 0; k < lightArray.size(); k++){
-        //define H for each light
+        //define vector H for each light source
         vector_H = vectorDivide(vectorAdd(L_array[k], vector_V), 2); //H = (L + V) /2
         vector_H = vectorDivide(vector_H, vectorLength(vector_H)); // H = H / ||H||
         normalize(vector_H);
 
+        //calculate the diffuse color
         ColorType diffuse = colorMultiply(material.Od, material.kd); //kd * od
-        float max_NL = max(0.0, dotProduct(vector_N, L_array[k]));
+        float max_NL = max(0.0, dotProduct(vector_N, L_array[k])); //clamp diffuse at 0
         diffuse = colorMultiply(diffuse, max_NL);
 
+        //calculate specular color
         ColorType specular = colorMultiply(material.Os, material.ks); //ks * Os
-        float max_NH = max(0.0, dotProduct(vector_N, vector_H));
+        float max_NH = max(0.0, dotProduct(vector_N, vector_H)); //clamp specular at 0
         specular = colorMultiply(specular, pow(max_NH, material.n)); //(ks * Os) * (N dot H)^n
-        //for each light, cast a ray to it and determine if it's in shadow(0) or not (1)
-
-        float light_dist = vectorLength(vectorSubtract(lightArray[k].position, intersection));
-        float f_att;
-
-        if(light_att == true){
-            f_att = 1/(attlight.c1 + (attlight.c2 * light_dist) + (attlight.c3* pow(light_dist, 2)));
-        }
-        else{
-            f_att = 1;
-        }
         
-        //Shadows
+        //distance from the sphere to the light source
+        float light_dist = vectorLength(vectorSubtract(lightArray[k].position, intersection));
+        
+        //Shadows - for each light, cast a ray to it and determine if it's in shadow(0) or not (1)
         int shadow_flag;
         Raytype shadow_ray;
-        shadow_ray.origin = intersection;
-        shadow_ray.intersection = vectorSubtract(lightArray[k].position, shadow_ray.origin);
+        shadow_ray.origin = intersection; //the surface intersection
+        shadow_ray.intersection = vectorSubtract(lightArray[k].position, shadow_ray.origin); //light position from the sphere surface
         normalize(shadow_ray.intersection);
-        //printVector(shadow_ray.intersection);
-        //printVector(shadow_ray.origin);
-        //normalize(shadow_ray.intersection);
-        //use traceRay to send a ray from the surface to the light sourcec
-        //if traceRay returns bakcgournd color, no objects are blocking it
-        tracing_shadow = true;
+        
+        //use traceRay to send a ray from the surface to the light source
         Intersection trace_ray = traceRay(shadow_ray, sphere_index);
         if(trace_ray.t < 0){
-            shadow_flag = 1;
+            shadow_flag = 1; //no intersection found, no shadow
         }
         else{
-
-            //the point where the shadow ray hit an object on its path to the light source
-            //VectorType blocking_point = vectorAdd(shadow_ray.origin, vectorScalar(shadow_ray.intersection, trace_ray.t));
             float blocking_dist = trace_ray.t;
-            //std::cout << t_shaded << std::endl;
-            //the distance from the sphere to the light source
+            //if light is directional, the object blocks the light
             if(lightArray[k].type == 0){
                 shadow_flag = 0;
-            } //if t is greater than the distance from the sphere to the light source, the object doesn't block the light
+            }//if t is greater than the distance from the sphere to a point light source, the object doesn't block the light
             else if(blocking_dist > light_dist){
                 shadow_flag = 1;
             }
-            else{ //point light and the object is betweent the light and the sphere
+            else{//point light and the object is blocking the light
                 shadow_flag = 0;
             }
         }
 
-        tracing_shadow = false;
-
+        //if there is no shadow, add the illumination from this light source to the total
         if(shadow_flag == 1){
             ColorType illum_L = colorMultiply(colorAdd(diffuse, specular), lightArray[k].intensity);
-            illum_L = colorMultiply(illum_L, f_att);
             illumination = colorAdd(illumination, illum_L);
             clampColor(illumination);
         }
 
     }
 
-    //depth cue
+    //if depth cue was input, compute depth cue
     if(depth_cue == true){
         if(t <= depth.dist_near){
             a_dc = depth.a_max;
@@ -407,7 +409,7 @@ ColorType shadeRay(int sphere_index, Raytype ray, float t){
         else{
             a_dc = depth.a_max - (depth.a_max - depth.a_min) * ((depth.dist_far - t)/(depth.dist_far - depth.dist_near));
         }
-
+        //compute illumination with depth cue
         illumination = colorAdd(colorMultiply(illumination, a_dc), colorMultiply(depth.depth_color, (1-a_dc)));
     }
 
@@ -415,15 +417,22 @@ ColorType shadeRay(int sphere_index, Raytype ray, float t){
     return illumination;
 }
 
-ColorType old_ShadeRay(int sphere_index){
-    SphereType sphere = sphereArray[sphere_index];
-    MaterialColor material = materialArray[sphere.m];
-    ColorType m_color = material.Od;
-    return m_color;
+ColorType oldShadeRay(int sphere_index, int t){
+    //std::cout << sphere_index << " " << t << std::endl;
+    if(t < 0){
+        return bkgcolor;
+    }
+    else{
+        int mat_index = sphereArray[sphere_index].m;
+        MaterialColor material = materialArray[mat_index];
+        ColorType color = material.Od;
+        return color;
+    }
 }
 
 //checks each object in the scene for a ray intersection and determines the closest valid intersection
-//either returns the color of the object it intersects or returns the background color if no object is intersected
+//skips the input self index if it's a valid index number
+//returns a struct containting the closest t-value, the intersected sphere index and the ray
 Intersection traceRay(Raytype ray, int self_index){
 
     //variables to track the closest t value and corresponding sphere/object
@@ -441,13 +450,11 @@ Intersection traceRay(Raytype ray, int self_index){
         float zc = sphereArray[k].center.k;
         float r = sphereArray[k].r;
         
-        
         //calculate A, B and C for each sphere
-        float A = 1.0;//pow(ray.intersection.i, 2) + pow(ray.intersection.j, 2) + pow(ray.intersection.k, 2);
+        float A = 1.0;
         float B = 2*(ray.intersection.i*(ray.origin.i - xc) + ray.intersection.j * (ray.origin.j - yc) + ray.intersection.k*(ray.origin.k - zc));
         float C = pow((ray.origin.i - xc), 2) + pow((ray.origin.j - yc), 2) + pow((ray.origin.k - zc), 2) - pow(r, 2);
         
-        //std::cout << ray.intersection.i << " " << ray.intersection.j << " " << ray.intersection.k << std::endl;
         //calculate the determinant
         float determinant = (pow(B, 2) - 4*A*C);
         
@@ -536,22 +543,10 @@ int main(int argc, const char * argv[]){
                 ColorType Os = {num4, num5, num6};
                 mtlcolor = {Od, Os, num7, num8, num9, num10};
                 materialArray.push_back(mtlcolor); //add new material to the material array
+                material_ = true;
             }
             else{
                 std::cerr << "Error parsing mtcolor line: " << line << std::endl;
-            }
-        }
-        else if (word == "attlight"){
-            if(ss >> num1 >> num2 >> num3 >> num4 >> num5 >> num6 >> num7 >> num8){
-                if(num4 != 1 && num4 != 0){
-                    std::cerr << "Error in attlight line, input 4: light type must be 0 (directional) or 1 (point)" << std::endl;
-                }
-                LightType a_light = {num1, num2, num3, static_cast<int>(num4), num5};
-                attlight = {a_light, num6, num7, num8};
-                light_att = true;
-            }
-            else{
-                std::cerr << "Error parsing attlight line: " << line << std::endl;
             }
         }
         else if (word == "depthcueing"){
@@ -566,6 +561,7 @@ int main(int argc, const char * argv[]){
         }
         else if (word == "light"){
             if (ss >> num1 >> num2 >> num3 >> num4 >> num5){
+                //check that the 4th input number is either 0 (directional light) or 1 (point light)
                 if(num4 != 1 && num4 != 0){
                     std::cerr << "Error in light line, input 4: light type must be 0 (directional) or 1 (point)" << std::endl;
                 }
@@ -583,16 +579,29 @@ int main(int argc, const char * argv[]){
                 VectorType center = {num1, num2, num3};
                 SphereType sphere = {center, num4, static_cast<int>(materialArray.size() - 1)};
                 sphereArray.push_back(sphere); //add spheres to the spere array
+                sphere_ = true;
             } else {
                 std::cerr << "Error parsing sphere line: " << line << std::endl;
             }
         }
         else if (word == "eye" || word == "viewdir" || word == "updir" || word == "bkgcolor") {
             if (ss >> num1 >> num2 >> num3) {
-                if (word == "eye")         eye = {num1, num2, num3};
-                if (word == "viewdir")     viewdir = {num1, num2, num3};
-                if (word == "updir")       updir = {num1, num2, num3};
-                if (word == "bkgcolor")    bkgcolor = {num1, num2, num3};
+                if (word == "eye"){
+                    eye = {num1, num2, num3};
+                    valid_input.eye_ = true;
+                }         
+                if (word == "viewdir"){
+                    viewdir = {num1, num2, num3};
+                    valid_input.viewdir_ = true;
+                }     
+                if (word == "updir"){
+                    updir = {num1, num2, num3};
+                    valid_input.updir_ = true;
+                }       
+                if (word == "bkgcolor"){
+                    bkgcolor = {num1, num2, num3};
+                    valid_input.bkgcolor_ = true;
+                }    
             } else {
                 std::cerr << "Error parsing " << word << " line: " << line << std::endl;
             }
@@ -601,6 +610,8 @@ int main(int argc, const char * argv[]){
             if (ss >> num1 >> num2) {
                 px_width = num1;
                 px_height = num2;
+                valid_input.px_height_ = true;
+                valid_input.px_width_ = true;
             } else {
                 std::cerr << "Error parsing imsize line: " << line << std::endl;
             }
@@ -608,6 +619,7 @@ int main(int argc, const char * argv[]){
         else if (word == "vfov") {
             if (ss >> num1) {
                vfov = num1;
+               valid_input.vfov_ = true;
             } else {
                 std::cerr << "Error parsing vfov line: " << line << std::endl;
             }
@@ -617,10 +629,40 @@ int main(int argc, const char * argv[]){
         }
     }
 
+    //check that all necessary input was given
+    if(!valid_input.eye_ || !valid_input.updir_|| !valid_input.viewdir_|| !valid_input.vfov_){
+        std::cerr << "Error: one or more viewer inputs (eye, view direction, updirection or vfov) was not given" << std::endl;
+        return -1;
+    }
+    else if(!valid_input.bkgcolor_){
+        std::cerr << "Error: No background color input" << std::endl;
+        return -1;
+    }
+    else if(!valid_input.px_height_ || !valid_input.px_width_){
+        std::cerr << "Error: imsize width and height were not properly input" << std::endl;
+        return -1;
+    }
+
     //create the header for the ppm file
     fout << "P3\n";
     fout << px_width << " # image width\n" << px_height << " # image height\n";
     fout << "255\n";
+
+    //if there is no input object, just return the background color
+    if(!sphere_){
+        for(int i = 0; i < px_height; i++){
+            for(int j = 0; j < px_width; j++){
+                fout << std::round(bkgcolor.r * 255) << " " << std::round(bkgcolor.g * 255) << " " << std::round(bkgcolor.b * 255) << std::endl;
+            }
+        }
+        inputFile.close();
+        return 0;
+    }//check that if there is a sphere, there is also a material color
+    else if(sphere_ && !material_){
+        std::cerr << "a sphere was input without a material color" << std::endl;
+        inputFile.close();
+        return -1;
+    }
     
     //PRELIMINARY MATH
 
@@ -691,11 +733,11 @@ int main(int argc, const char * argv[]){
                 std::cerr << "Unable to normalize" << std::endl;
             }
     
-            //use trace ray to find the color for each pixel
+            //use trace_ray and shade ray to determine the color for each pixel
             Intersection trace_ray = traceRay(ray, -1);
             ColorType color = shadeRay(trace_ray.sphere_index, trace_ray.ray, trace_ray.t);  
-            
             fout << std::round(color.r * 255) << " " << std::round(color.g * 255) << " " << std::round(color.b * 255) << std::endl;
+            
         }
     }
 
